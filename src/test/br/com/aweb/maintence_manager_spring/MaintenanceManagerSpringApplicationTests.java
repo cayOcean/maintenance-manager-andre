4282b4da90267505eb29b65403d08ca9295b54d4
cayOcean <cayquelima4@gmail.com>
2025-09-26 20:24:59 -0300

    subindo erros

--- src/test/br/com/aweb/maintence_manager_spring/MaintenanceManagerSpringApplicationTests.java
+++ src/test/br/com/aweb/maintence_manager_spring/MaintenanceManagerSpringApplicationTests.java
@@ -3,12 +3,11 @@ package br.com.aweb.maintenance_manager_spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest // Inicializa o contexto do Spring Boot para testes
+@SpringBootTest
 class MaintenanceManagerSpringApplicationTests {
 
-    @Test
-    void contextLoads() {
-        // Teste básico para verificar se o contexto da aplicação carrega sem erros
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
